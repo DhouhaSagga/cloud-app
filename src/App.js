76c77e29fe1df6c6c4c0e08ce56ae618b03a3857
dhouhasagga <saggadhouha@gmail.com>
2023-05-16 17:26:23 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 function App() {
+  const item = {
+    name: "Location 1",
+    location: {
+      lat: 33.87239440050665, 
+      lng: 10.109997496425345,
+    },
+  };
+
+  const mapStyles = {
+    height: "400px",
+    width: "100%",
+  };
+
+  const defaultCenter = {
+    lat: 33.87239440050665,
+    lng: 10.109997496425345,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="aboutUsElement">
+        <LoadScript googleMapsApiKey="AIzaSyC-9HBtGH2Tn5omz6Y4g9B29Jg5EVVx2xE">
+          <GoogleMap
+            mapContainerStyle={mapStyles}
+            zoom={15}
+            center={defaultCenter}
+          >
+            <Marker key={item.name} position={item.location} />
+          </GoogleMap>
+        </LoadScript>
+      </div>
     </div>
   );
 }
